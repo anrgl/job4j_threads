f8dcb5ff6f21fc3adebb9633b8fa6b136682244f
aynur <galimovaynur@protonmail.com>
2021-01-28 18:10:26 +0500

    1.1. Состояние нити. [#450502] v2

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -12,14 +12,13 @@ public class ThreadState {
         System.out.println("second: name: " + second.getName() + " state: " + second.getState());
         first.start();
         second.start();
-        while (first.getState() != Thread.State.TERMINATED) {
+        while (first.getState() != Thread.State.TERMINATED && second.getState() != Thread.State.TERMINATED) {
             System.out.println("first: name: " + first.getName() + " state: " + first.getState());
-        }
-        while (second.getState() != Thread.State.TERMINATED) {
             System.out.println("second: name: " + second.getName() + " state: " + second.getState());
         }
         System.out.println("first: name: " + first.getName() + " state: " + first.getState());
         System.out.println("second: name: " + second.getName() + " state: " + second.getState());
+        System.out.println("current: name: " + Thread.currentThread().getName() + " state: " + Thread.currentThread().getState());
         System.out.println("Work is end!");
     }
 }
