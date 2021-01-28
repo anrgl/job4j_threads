517aa65b179533514fcea9b12561e519be5574d4
aynur <galimovaynur@protonmail.com>
2021-01-28 23:46:37 +0500

    3.1. Прерывание блокированной нити. [#450504]

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -1,24 +1,17 @@
 package ru.job4j.concurrent;
 
-import java.util.Random;
-
 public class Wget {
     public static void main(String[] args) {
-        Random random = new Random();
-
-        Thread thread = new Thread(
-            () -> {
+        Thread thread = new Thread(() -> {
+            for (int i = 1; i <= 100; i++) {
+                System.out.print("\rLoading: " + i + "%");
                 try {
-                    for (int i = 1; i <= 100; i++) {
-                        random.setSeed(System.currentTimeMillis());
-                        System.out.print("\rLoading: " + i + "%");
-                        Thread.sleep(random.nextInt(2000));
-                    }
+                    Thread.sleep(100);
                 } catch (InterruptedException e) {
-                    e.printStackTrace();
+                    Thread.currentThread().interrupt();
                 }
             }
-        );
+        });
         thread.start();
     }
 }
