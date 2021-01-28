c6ed7e6349bc9cf603867a5be31cf1deebff642a
aynur <galimovaynur@protonmail.com>
2021-01-28 23:00:52 +0500

    Refactored long lines

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -8,17 +8,25 @@ public class ThreadState {
         Thread second = new Thread(
             () -> System.out.println(Thread.currentThread().getName())
         );
-        System.out.println("first: name: " + first.getName() + " state: " + first.getState());
-        System.out.println("second: name: " + second.getName() + " state: " + second.getState());
+        System.out.println("first: name: " + first.getName()
+            + " state: " + first.getState());
+        System.out.println("second: name: " + second.getName()
+            + " state: " + second.getState());
         first.start();
         second.start();
-        while (first.getState() != Thread.State.TERMINATED && second.getState() != Thread.State.TERMINATED) {
-            System.out.println("first: name: " + first.getName() + " state: " + first.getState());
-            System.out.println("second: name: " + second.getName() + " state: " + second.getState());
+        while (first.getState() != Thread.State.TERMINATED
+            && second.getState() != Thread.State.TERMINATED) {
+            System.out.println("first: name: " + first.getName()
+                + " state: " + first.getState());
+            System.out.println("second: name: " + second.getName()
+                + " state: " + second.getState());
         }
-        System.out.println("first: name: " + first.getName() + " state: " + first.getState());
-        System.out.println("second: name: " + second.getName() + " state: " + second.getState());
-        System.out.println("current: name: " + Thread.currentThread().getName() + " state: " + Thread.currentThread().getState());
+        System.out.println("first: name: " + first.getName()
+            + " state: " + first.getState());
+        System.out.println("second: name: " + second.getName()
+            + " state: " + second.getState());
+        System.out.println("current: name: " + Thread.currentThread().getName()
+            + " state: " + Thread.currentThread().getState());
         System.out.println("Work is end!");
     }
 }
